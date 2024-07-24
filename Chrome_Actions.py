c40b5efc4c56729057cd30ce61b40934b34f46ca
Grant L <grant.jy.li@gmail.com>
2024-07-24 13:11:06 -0400

    added Wait time and tag

--- Chrome_Actions.py
+++ Chrome_Actions.py
@@ -24,10 +24,11 @@ options = Options()
 options.add_experimental_option("detach", True) #leaves window open when done
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+waitTime = tagData["elementWaitTime"] # seconds
 #helper function to get elements that have to be waited
 def waitElementCSS(tag):
     try:
-        return WebDriverWait(driver, 10).until(
+        return WebDriverWait(driver, waitTime).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, tag))
             )
     except TimeoutException or NoSuchElementException:
@@ -39,11 +40,11 @@ def loginInstagram():
     driver.get(instagramURL) # opening up instagram
 
     #entering credentials and logging in
-    unInput = waitElementCSS(tagData["UNInputCSS"])
+    unInput = waitElementCSS(tagData["unINPUT"])
     if unInput:
 
-        pwInput = driver.find_element(By.CSS_SELECTOR, tagData["PWInputCSS"])
-        loginBTN = driver.find_element(By.CSS_SELECTOR, tagData["loginBTNCSS"])
+        pwInput = driver.find_element(By.CSS_SELECTOR, tagData["pwINPUT"])
+        loginBTN = driver.find_element(By.CSS_SELECTOR, tagData["loginBTN"])
 
         unInput.send_keys(accData["accUsername"])
         pwInput.send_keys(accData["accPassword"])
@@ -56,7 +57,7 @@ def loginInstagram():
 
     #================THEY SUSPECT AUTOMATED BEHAVIOR=============
     #dismiss button
-    dismissBTN = waitElementCSS(tagData["dismissBTNCSS"])
+    dismissBTN = waitElementCSS(tagData["dismissBTN"])
     if dismissBTN:
         dismissBTN.click()
         print("Dismissed Automated behavior")
@@ -66,7 +67,7 @@ def loginInstagram():
     #============================================================
 
     # "save login" info pop-up might happen
-    notNowBTN = waitElementCSS(tagData["notNowLoginBTNCSS"])
+    notNowBTN = waitElementCSS(tagData["notNowloginBTN"])
     if notNowBTN:
         notNowBTN.click()
         print("No save Login")
@@ -75,7 +76,7 @@ def loginInstagram():
         return
 
     # "not-now" info pop-up might happen
-    notNowNotifBTN = waitElementCSS(tagData["notNowNotifBTNCSS"])
+    notNowNotifBTN = waitElementCSS(tagData["notNowNotifBTN"])
     if notNowNotifBTN:
         notNowNotifBTN.click()
         print("No notifications please")
@@ -86,7 +87,7 @@ def loginInstagram():
 
 def uploadMedia():
     print("Uploading...") # get upload button
-    uploadBTN = driver.find_element(By.CSS_SELECTOR, tagData["postIcon"])
+    uploadBTN = driver.find_element(By.CSS_SELECTOR, tagData["postIconSVG"])
 
     for name in os.listdir("Media"):
         filePath = os.path.join(os.getcwd(), "Media", name)
@@ -103,7 +104,7 @@ def uploadMedia():
             elif fileType == '.jpg':
                 images.append(os.path.join(filePath, file))
 
-        fileUpload = waitElementCSS(tagData["fileInput"])
+        fileUpload = waitElementCSS(tagData["fileINPUT"])
 
         if len(videos) > 0:
             print(videos)
@@ -117,18 +118,15 @@ def uploadMedia():
         if reelsBTN:
             reelsBTN.click()
 
-        uploadBTN = waitElementCSS(tagData["uploadNextBTN"])
-
-        if uploadBTN:
-            uploadBTN.click()
-            uploadBTN.click()
-            uploadBTN.click()
-        else:
-            print("Cannot upload")
-            return
+        for i in range(3):
+            uploadBTN = waitElementCSS(tagData["uploadNextBTN"])
+            if uploadBTN:
+                uploadBTN.click()
+            else:
+                print("Cannot upload: phase #" + str(i))
+                return
 
         closeSVG = waitElementCSS(tagData["closeUploadSVG"])
-
         if closeSVG:
             closeSVG.click()
 
