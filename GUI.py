8feabd4dd62972ca67c817ac8d6256e268eba239
Grant L. <56404949+GrantjyLi@users.noreply.github.com>
2024-08-01 15:07:41 -0400

    done GUI design
    very rudimentary, now implement functionality

--- GUI.py
+++ GUI.py
@@ -1,22 +1,57 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.geometry("700x500")
-window.title("Instagram Reposter")
+class GUI:
+    def __init__(self):
 
-HEADER_SIZE = 15
-TEXT_SIZE = 12
+        window = tk.Tk()
+        window.geometry("700x500")
+        window.title("Instagram Reposter")
 
-nameslabel = tk.Label(window, text = "Victim Account Names:", font=('Arial', HEADER_SIZE))
-nameslabel.place(x=10, y=10)
+        HEADER_SIZE = 15
+        TEXT_SIZE = 12
 
-accNames = tk.Text(window, font=('Arial', TEXT_SIZE))
-accNames.place(x=10, y = 35, width= 300, height=150)
+        nameslabel = tk.Label(window, text = "Victim Account Names:", font=('Arial', HEADER_SIZE))
 
-# unEntry = tk.Entry(window, text = "Profile Account Names:", font=('Arial', HEADER_SIZE))
-# pwEntry = tk.Entry(window, text = "Profile Account Names:", font=('Arial', HEADER_SIZE))
-# unEntry.pack()
-# pwEntry.pack()
+        accNames = tk.Text(window, font=('Arial', TEXT_SIZE))
+
+        unEntry = tk.Entry(window, font=('Arial', TEXT_SIZE))
+        pwEntry = tk.Entry(window, font=('Arial', TEXT_SIZE))
+
+        unlabel = tk.Label(window, text = "Enter Account Username:", font=('Arial', HEADER_SIZE))
+        pwlabel = tk.Label(window, text = "Enter Account Password:", font=('Arial', HEADER_SIZE))
+
+        nameslabel.place(x = 10, y=10)
+        accNames.place(x = 10, y = 35, width= 300, height=150)
+        unlabel.place(x = 10, y = 195)
+        unEntry.place(x = 10, y = 230)
+        pwlabel.place(x = 10, y = 250)
+        pwEntry.place(x = 10, y = 270)
 
 
-window.mainloop()
+        postDescLabel = tk.Label(window, text = "Automated Post Description:", font=('Arial', HEADER_SIZE))
+        postDesc = tk.Text(window, font=('Arial', TEXT_SIZE))
+
+        grabContent = tk.Checkbutton(window, text = "Grab Content", font=('Arial', TEXT_SIZE))
+        autoUpload = tk.Checkbutton(window, text = "Automated Upload", font=('Arial', TEXT_SIZE))
+        ecoMode = tk.Checkbutton(window, text = "Economy Mode", font=('Arial', TEXT_SIZE))
+
+        autoUpload.select()
+        grabContent.select()
+        ecoMode.select()
+
+        startBTN = tk.Button(window, text = "Start", font=('Arial', HEADER_SIZE))
+
+        postDescLabel.place(x = 335, y=10)
+        postDesc.place(x = 335, y = 35, width= 350, height=175)
+        grabContent.place(x = 335, y = 215)
+        autoUpload.place(x = 335, y = 240)
+        ecoMode.place(x = 335, y = 265)
+        startBTN.place(x = 520, y = 230, width = 125, height = 50)
+
+        outputLabel = tk.Label(window, text = "Program Output", font=('Arial', HEADER_SIZE))
+        output = tk.Text(window, font=('Arial', TEXT_SIZE))
+
+        outputLabel.place(x = 290, y = 290)
+        output.place(x = 10, y = 320, width = 680, height = 170)
+
+        window.mainloop()
